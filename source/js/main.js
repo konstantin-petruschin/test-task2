3fc9583bb8ef5d1719173a69ca60d9599033c99c
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2025-02-21 23:21:52 +0300

    Адаптив для планшета и  проверка на валидацию

--- source/js/main.js
+++ source/js/main.js
@@ -6,5 +6,11 @@ const menuElement = document.querySelector('.header__nav');
 toogleButtonElement.addEventListener('click', function () {
   toogleButtonElement.classList.toggle('header__toggle--closed');
   menuElement.classList.toggle('header__nav--open');
-})
+
+  if (menuElement.classList.contains('header__nav--open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
 
