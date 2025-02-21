71ac3953f0e69c93fc40a72cd7c843e493dfbcf6
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2025-02-21 14:05:29 +0300

    адаптив мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,42 +1,10 @@
 // Выпадающее меню
 
-const toogleButtonElement = document.querySelector('.main-header__toggle');
-const menuElement = document.querySelector('.main-nav');
-const nojsElement = document.querySelector('.no-js');
-nojsElement.classList.remove('no-js');
-
+const toogleButtonElement = document.querySelector('.header__toggle');
+const menuElement = document.querySelector('.header__nav');
 
 toogleButtonElement.addEventListener('click', function () {
-  toogleButtonElement.classList.toggle('main-header__toggle--closed');
-  menuElement.classList.toggle('main-nav--opened');
+  toogleButtonElement.classList.toggle('header__toggle--closed');
+  menuElement.classList.toggle('header__nav--open');
 })
 
-
-const mapWrapper = document.querySelector('#map');
-
-if (mapWrapper) {
-  ymaps.ready(init);
-}
-
-function init() {
-  const myMap = new ymaps.Map('map', {
-    center: [59.93873, 30.32285],
-    zoom: 17
-  });
-
-  let imageSource = {
-    src: '../img/icons/map-pin.svg',
-    size: [67, 100],
-    offset: [-40, -100]
-  }
-
-  myMap.geoObjects.add(new ymaps.Placemark([59.938631, 30.323037],
-    {
-      hintContent: '<div class="map__hint">Магазин «Мишка»</div>'
-    }, {
-      iconLayout: 'default#image',
-      iconImageHref: imageSource.src,
-      iconImageSize: imageSource.size,
-      iconImageOffset: imageSource.offset
-    }))
-}
